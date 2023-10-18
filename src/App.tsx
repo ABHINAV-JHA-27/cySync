e36c4aca8888b09b317b94afaf6cb2df89df4e3b
ABHINAV JHA <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2023-10-18 09:55:06 +0530

    feat: (navigation) Setup Navigation for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import {
+    Navigate,
+    Route,
+    BrowserRouter as Router,
+    Routes,
+    redirect,
+} from "react-router-dom";
+import styled from "styled-components";
+import Layout from "./container/Layout";
+import "./index.css";
+
+const AppContainer = styled.div`
+    flex: 1;
+    height: 100vh;
+    width: 100vw;
+`;
+
 function App() {
-    return <div></div>;
+    return (
+        <AppContainer>
+            <Router>
+                <Layout>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<Navigate to="/wallets" replace={true} />}
+                        />
+                        <Route path="/wallets" element={<h1>heelo</h1>} />
+                        <Route path="/transactions" element={<h1>hii</h1>} />
+                    </Routes>
+                </Layout>
+            </Router>
+        </AppContainer>
+    );
 }
 
 export default App;
